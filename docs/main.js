4663d9da9073ab6513ee95d55689740a34df2aa0
EmzdZiddan <emzdziddan@gmail.com>
2025-08-24 19:23:37 +0700

    oke

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,24 @@
-gsap.from(".hero-text", {
-  duration:  2,
-  scale:1,
-  opacity:0,
-});
-
 gsap.to(".orn1", {
   duration: 2,
   repeat:Infinity,
   rotation:360,
   scale:2,
 });
+
+
+let tl = gsap.timeline();
+
+tl.from(".hero-text > *", {
+  duration: 1,
+  opacity: 0,
+  y: 30,
+  stagger: 0.2
+})
+.from(".zid img", {   // foto masuk setelah teks
+  duration: 1.2,
+  opacity: 0,
+  x: 40
+}, "-=2"); // mulai 0.5 detik sebelum animasi teks selesai
 
 
 document.addEventListener("DOMContentLoaded", function () {
